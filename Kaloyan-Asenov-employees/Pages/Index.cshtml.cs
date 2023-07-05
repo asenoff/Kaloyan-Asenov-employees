8bb1b64f5a4567b7c3aa07898eaeecdd1745d380
Kaloyan Asenov <kaloyan@asenoff.net>
2023-07-05 19:10:07 +0300

    Adjusting file preview max size for UI

--- Kaloyan-Asenov-employees/Pages/Index.cshtml.cs
+++ Kaloyan-Asenov-employees/Pages/Index.cshtml.cs
@@ -13,7 +13,7 @@ namespace Kaloyan_Asenov_employees.Pages
 
         private readonly IAllCoworkers _allCoworkers;
 
-        private const int FILE_CONTENT_MAX_LENGTH = 2000;
+        private const int FILE_CONTENT_MAX_LENGTH = 200;
 
         internal string LastErrorMessage { get; set; } = string.Empty;
 
