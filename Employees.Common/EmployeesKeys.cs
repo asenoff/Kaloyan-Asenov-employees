dc54bf8002ea90238d2e9b29d498083a6d8e1a3c
Kaloyan Asenov <kaloyan@asenoff.net>
2023-07-05 09:24:29 +0300

    Fixing bitwise operators bug in employee pair key processing

--- Employees.Common/EmployeesKeys.cs
+++ Employees.Common/EmployeesKeys.cs
@@ -2,6 +2,7 @@
 {
     public class EmployeesKeys
     {
+        private const int BITS_IN_BYTE = 8;
         public static ulong GetCompositeEmployeesKey(uint employeeKey1, uint employeeKey2)
         {
             if(employeeKey1 == employeeKey2)
@@ -14,23 +15,23 @@
                 throw new ArgumentException("employee keys should both be non-zero");
             }
 
-            var keyLength = sizeof(uint);
+            var keyLength = sizeof(uint) * BITS_IN_BYTE;
             ulong result;
             if(employeeKey1 < employeeKey2)
             {
-                result = (employeeKey1 << keyLength) & employeeKey2;
+                result = ((ulong)employeeKey1 << keyLength) | employeeKey2;
                 return result;
             }
             else
             {
-                result = (employeeKey2 << keyLength) & employeeKey1;
+                result = ((ulong)employeeKey2 << keyLength) | employeeKey1;
                 return result;
             }
         }
 
         public static (uint, uint) GetEmployeeIDs(ulong compositeKey)
         {
-            var idLength = sizeof(uint);
+            var idLength = sizeof(uint) * BITS_IN_BYTE;
             var id1 = (uint) (compositeKey >> idLength);
             var id2 = (uint) (compositeKey & uint.MaxValue);
 
